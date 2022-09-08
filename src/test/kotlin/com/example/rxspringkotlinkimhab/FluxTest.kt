780e55c171c7315dc8199658cc91fd8291e34ee5
kimhab <69948342+kimhab11@users.noreply.github.com>
2022-09-08 15:40:19 +0700

    Initial commit

--- src/test/kotlin/com/example/rxspringkotlinkimhab/FluxTest.kt
+++ src/test/kotlin/com/example/rxspringkotlinkimhab/FluxTest.kt
@@ -1,4 +1,32 @@
 package com.example.rxspringkotlinkimhab
 
+import org.junit.jupiter.api.Test
+import reactor.core.publisher.Flux
+import reactor.test.StepVerifier
+
 class FluxTest {
+    @Test
+    fun flux1(){
+        val  flux = Flux.just("A","B","V").log()
+
+        StepVerifier.create(flux)
+            .expectNext("A") //.expectNext("A","B","V")
+            .expectNext("B")
+            .expectNext("V")
+            .verifyComplete()
+    }
+    @Test
+    fun flux2(){
+        val  flux = Flux.just("A","B","C")
+            .concatWith(Flux.error(RuntimeException("Error have same @")))
+            .log()
+
+        StepVerifier.create(flux)
+            .expectNext("A") //.expectNext("A","B","V")
+            .expectNext("B")
+            .expectNext("C")
+            .expectErrorMessage("Error have same @")
+            // .expectError(RuntimeException::class.java)
+            .verify()
+    }
 }
--- src/test/kotlin/com/example/rxspringkotlinkimhab/MonoTest.kt
+++ src/test/kotlin/com/example/rxspringkotlinkimhab/MonoTest.kt
@@ -1,4 +1,40 @@
 package com.example.rxspringkotlinkimhab
 
+import org.junit.jupiter.api.Test
+import reactor.core.publisher.Mono
+import reactor.test.StepVerifier
+
+
+// Mono can have only 1 or 0 data
 class MonoTest {
+    @Test
+    fun mono1(){
+        Mono.just("AA")
+            .log()
+            .subscribe({data -> println("My data is $data") })
+    }
+    @Test
+    fun mono2(){
+        Mono.error<Exception>(Exception("Have exception"))
+            .log()
+            .doOnError { error -> println("error merge  ${error.message}") }
+            .subscribe({},{error -> println("error merge  ${error.message}") })
+    }
+
+    @Test
+    fun junitTest1(){
+        val a = Mono.just("A")
+            .log()
+        StepVerifier.create(a)
+            .expectNext("A")
+            .verifyComplete()
+    }
+    @Test
+    fun junitTest2(){
+        val a = Mono.error<Exception>(Exception("Some exception"))
+            .log()
+        StepVerifier.create(a)
+            .expectError(Exception::class.java)
+            .verify()
+    }
 }
